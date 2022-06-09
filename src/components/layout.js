18b74fa62bca1d95ba493be621fd5e1040be6466
eribloot <Fenton_Eric@pm.me>
2022-06-09 11:55:29 -0500

    added css

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,20 +2,27 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
+//import css
+import { container, heading, navLinks, navLinkItem, navLinkText } from "./layout.module.css"
+
 //declare layout to group all elements together for easy updates
 const Layout = ({ pageTitle, children }) => {
   return(
-    <div>
+    <div className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>Home</Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>About</Link>
+          </li>
         </ul>
       </nav>
 
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className ={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
